4ddde5e7a72e281b63cc578d741b914157753cfe
axhse <asbityugov@edu.hse.ru>
2024-12-31 12:41:46 +0300

    minimalistic web app

--- src/main/java/com/example/cellfire/Application.java
+++ src/main/java/com/example/cellfire/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CellfireApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CellfireApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
