7192c2a02d035bf7facd12f75be57a1c2e37e7fa
axhse <asbityugov@edu.hse.ru>
2024-12-31 12:30:21 +0300

    create Spring Initializr app

---
+++ src/main/java/com/example/cellfire/CellfireApplication.java
@@ -0,0 +1,13 @@
+package com.example.cellfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cellfire/CellfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cellfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
